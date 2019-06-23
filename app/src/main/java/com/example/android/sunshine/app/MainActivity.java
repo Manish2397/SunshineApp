9c256dad086eeb2915eaadbc242417132ca03be0
manish2397 <rawatmanish2397@gmail.com>
2019-06-23 12:20:39 +0530

    httpRequest Added commit

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.sunshine.app;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,6 +12,12 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 
@@ -22,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -45,32 +52,15 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
+        if(id==R.id.action_refresh){
+            ForecastFragment.FetchWeatherTask weatherTask = new ForecastFragment.FetchWeatherTask();
+            weatherTask.execute();
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            ArrayList<String> data=new ArrayList<String>();
-            data.add("manish");
-            data.add("rohit");
-            data.add("rohit");
-            data.add("rohit");
-            data.add("rohit");
-            data.add("rohit");
-            ArrayAdapter<String>  dataAdapter = new ArrayAdapter<String>(getActivity(),R.layout.list_item_forecast,R.id.item_forecast_listview,data);
-            ListView listView = findViewById
-            return rootView;
-        }
-    }
 }
